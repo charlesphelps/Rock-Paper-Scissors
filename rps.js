cd8e8402cad1b499248d5518cfedf1417827f16d
Charlie Phelps <129246710+charlesphelps@users.noreply.github.com>
2023-07-19 14:39:49 -0500

    Create game function and define for loop parameter

--- rps.js
+++ rps.js
@@ -1,4 +1,7 @@
-
+let playerScore = 0;
+let compScore = 0;
+const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
 
@@ -13,37 +16,40 @@ function getComputerChoice() {
     }
 }
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Choose what to throw', 'Rock, Paper, Scissors').toLowerCase();
+        const computerSelection = getComputerChoice;
+        function playRound (playerSelection,computerSelection) {
 
-function playRound (playerSelection,computerSelection) {
-    console.log(playerSelection);
-
-    if (playerSelection === computerSelection) {
-        return "Draw!";
+            if (playerSelection === computerSelection) {
+                return "Draw!";
+            }
+        
+            if (playerSelection === "rock") {
+                if (computerSelection === "paper") {
+                    return "You lose! Paper beats rock!";
+                } else (computerSelection === "scissors")
+                    return "You win! Rock beats scissors!";
+            }
+            if (playerSelection === "paper") {
+                if (computerSelection === "scissors") {
+                    return "You lose! Scissors beats paper!";
+                } else (computerSelection === "rock")
+                    return "You win! Paper beats rock!";
+            }
+        
+            if (playerSelection === "scissors") {
+                if (computerSelection === "rock") {
+                    return "You lose! Rock beats scissors!";
+            } else (computerSelection === "paper")
+                return "You win! Scissors beats paper!";
+            }
+        }
     }
 
-    if (playerSelection === "rock") {
-        if (computerSelection === "paper") {
-            return "You lose! Paper beats rock!";
-        } else (computerSelection === "scissors")
-            return "You win! Rock beats scissors!";
     }
 
-    if (playerSelection === "paper") {
-        if (computerSelection === "scissors") {
-            return "You lose! Scissors beats paper!";
-        } else (computerSelection === "rock")
-            return "You win! Paper beats rock!";
-    }
-    
-    if (playerSelection === "scissors") {
-        if (computerSelection === "rock") {
-            return "You lose! Rock beats scissors!";
-        } else (computerSelection === "paper")
-            return "You win! Scissors beats paper!";
-    }
+// code below is for validating playRound function
+//console.log(playRound(playerSelection, computerSelection));
 
-}
-
-const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
