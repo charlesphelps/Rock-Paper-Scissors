882989554d43e53c932daa22f3e9170494eb1545
Charlie Phelps <129246710+charlesphelps@users.noreply.github.com>
2023-07-25 17:24:51 -0500

    Create three buttons for rps, with js alerts fired when clicked. Wrap js in window.onload function

--- rps.js
+++ rps.js
@@ -1,62 +1,10 @@
-let playerScore = 0;
-let compScore = 0;
-let arr = ["rock", "paper", "scissors"];
-let computerSelection;
+window.onload = function(){ 
+const btnOne = document.querySelector('#btnOne');
+btnOne.onclick = () => alert("Hello World");
 
-let getComputerChoice = () => {
-    return computerSelection = (arr[(Math.floor(Math.random() * arr.length))])
+
+const btnTwo = document.querySelector('#btnTwo');
+btnTwo.addEventListener('click', () => {
+  alert("Hello World");
+});
 };
-
-let playRound = (playerSelection,computerSelection) => {
-
-    if (playerSelection === computerSelection) {
-        console.log("draw");
-        return playerScore + 0;
-    }
-    if (playerSelection === "rock") {
-        if (computerSelection === "paper") {
-            console.log("paper beats rock");
-            return ++compScore;
-        } else (computerSelection === "scissors")
-            console.log("rock beats scissors");
-            return ++playerScore;
-    }
-    if (playerSelection === "paper") {
-        if (computerSelection === "scissors") {
-            console.log("scissors beats paper");
-            return ++compScore;
-        } else (computerSelection === "rock")
-            console.log("paper beats rock");
-            return ++playerScore;
-    }
-
-    if (playerSelection === "scissors") {
-        if (computerSelection === "rock") {
-            console.log("rock beats scissors");
-            return ++compScore;
-    } else (computerSelection === "paper")
-        console.log("scissors beats paper");
-        return ++playerScore;
-    }
-};
-
-
-const game = () => {
-    for (let i = 1; i < 6; i++) {
-        let playerSelection = prompt('Choose what to throw', "").toLowerCase();
-        getComputerChoice();
-        playRound(playerSelection,computerSelection);
-        console.log("Round "+ i + ": " + playerScore + " to " + compScore + ".");
-    }
-
-    if (playerScore > compScore) {
-        console.log("you win");
-    } else if (playerScore < compScore) {
-        console.log("you lose");
-    } else {
-        console.log("tie");;
-    }
-
-    };
-
-    game();
